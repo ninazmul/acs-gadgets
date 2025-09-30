bf0ca09a9f9423d125281117b7ee4b142fc7efa2
ninazmul <nazmulsaw@gmail.com>
2025-09-30 11:07:16 +0600

    products page updated

--- app/(root)/products/page.tsx
+++ app/(root)/products/page.tsx
@@ -2,14 +2,11 @@ import ProductFiltersClient from "@/components/shared/ProductFiltersClient";
 import { getAllProducts } from "@/lib/actions/product.actions";
 
 export default async function ProductPage() {
-
-  const products = (await getAllProducts()) || [];
+  const products = await getAllProducts();
 
   return (
-    <div >
-      <ProductFiltersClient
-        rawProducts={products}
-      />
+    <div>
+      <ProductFiltersClient rawProducts={products} />
     </div>
   );
 }
