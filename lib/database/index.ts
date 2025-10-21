3edb392b4937428a243390394f3b863f6737a6c7
ninazmul <nazmulsaw@gmail.com>
2025-10-21 16:34:55 +0600

    db name changed to acsGadgets

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -13,7 +13,7 @@ export const connectToDatabase = async () => {
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URI, {
-      dbName: "drop&shipping",
+      dbName: "acsGadgets",
       bufferCommands: false,
     });
 
